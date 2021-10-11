9d0d0664136a1da7de9436146a52441e20754a1a
no <2chainzxsoft@gmail.com>
2021-10-11 21:54:11 +0300

    db implementation

--- dbmanager/dbmanager.go
+++ dbmanager/dbmanager.go
@@ -1,26 +1,50 @@
 package dbmanager
 
 import (
-	"database/sql"
 	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
-var hostname string
-var username string
-var password string
-var dbName string
+type DBEntity struct {
+	ID          int    `json:"id" db:"id"`
+	Name        string `json:"Name" db:"Name"`
+	Description string `json:"Description" db:"Description"`
+	Status      string `json:"Status" db:"Status"`
+}
 
-var DBCon *sql.DB
+var hostname string = "95.214.55.115"
+var username string = "testdb"
+var password string = "2pRSTXAMBh5wLMtF"
+var dbName string = "testdb"
 
-func connect() {
+var DBCon *sqlx.DB
+
+func Connect() {
 	var err error
 	//username:password@tcp(host:port)/dbname
 	connStr := fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, hostname, dbName)
 	fmt.Println("Establishing connection " + connStr)
-	DBCon, err = sql.Open("mysql", connStr)
+	DBCon, err = sqlx.Open("mysql", connStr)
 
 	if err != nil {
-		panic(err.Error())
+		fmt.Println(err.Error())
 	}
+
+	if err = DBCon.Ping(); err != nil {
+		DBCon.Close()
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Connected")
+
+	entity := []DBEntity{}
+	err = DBCon.Select(&entity, "SELECT * FROM pendingData LIMIT 1")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Printf("%+v\n", entity)
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module sharedcrud
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jmoiron/sqlx v1.3.4 // indirect
+)
